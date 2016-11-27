b58163a864a791073194e57a30a665933d0c9b62
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-11-27 20:29:25 +0100

    Added csharp project scaffold

---
+++ csharp/TestingExercises/TestingExercises/ExampleTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace TestingExercises
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        public void WorldIsSane()
+        {
+            Assert.IsTrue(true);
+        }
+
+        public interface Foo
+        {
+            int Bar();
+        }
+
+        private class Baz
+        {
+            private readonly Foo _foo;
+
+            public Baz(Foo foo)
+            {
+                _foo = foo;
+            }
+
+            public bool Qux()
+            {
+                return _foo.Bar() == 1337;        
+            }
+        }
+
+        [TestMethod]
+        public void NSubstituteExample()
+        {
+            var aFoo = Substitute.For<Foo>();
+            aFoo.Bar().Returns(1337);
+
+            var aBaz = new Baz(aFoo);
+
+            Assert.IsTrue(aBaz.Qux());
+        }
+    }
+}
