017e672538b99745ac666830c7eee2b67ba22ad4
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-11-28 07:59:42 +0100

    calculator port

---
+++ csharp/TestingExercises/TestingExercises/test/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingExercises.test
+{
+
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void CanAdd()
+        {
+            Assert.Fail("Spec add behaviour.");
+        }
+
+        [TestMethod]
+        public void CanMultiply()
+        {
+            Assert.Fail("Spec multiply behaviour.");
+        }
+    }
+}
