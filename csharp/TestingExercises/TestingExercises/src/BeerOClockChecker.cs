b3c7aa74dbe91b3ebc7045f12eaf5a1cb4e135ee
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-11-28 07:54:36 +0100

    ported beer o clock

---
+++ csharp/TestingExercises/TestingExercises/src/BeerOClockChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestingExercises
+{
+    public class BeerOClockChecker
+    {
+        private const int BeerOClock = 16;
+
+        public bool IsBeerOClock()
+        {
+            return Now().Hour >= BeerOClock;
+        }
+
+        protected virtual DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ csharp/TestingExercises/TestingExercises/test/BeerOClockCheckerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TestingExercises.test
+{
+    public class BeerOClockCheckerTest
+    {
+        [Fact]
+        public void KnowsWhenItIsBeerOClock()
+        {
+            throw new NotImplementedException("TODO: write specs for the checker.");
+        } 
+    }
+}
