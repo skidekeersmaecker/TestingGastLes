7c5a607837fa1051011224ccfc23a2767abbc44f
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-11-28 07:50:54 +0100

    added xunit. and example teory. without VS blowing up because of multiple test runners

---
+++ csharp/TestingExercises/TestingExercises/src/Fibonacci.cs
@@ -0,0 +1,13 @@
+﻿namespace TestingExercises
+{
+    public class Fibonacci
+    {
+        public static int Compute(int input)
+        {
+            if (input == 0) return 0;
+            if (input == 1) return 1;
+
+            return Compute(input - 1) + Compute(input - 2);
+        }
+    }
+}
---
+++ csharp/TestingExercises/TestingExercises/test/ParametrizedTestExample.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace TestingExercises.test
+{
+    public class ParametrizedTestExample
+    {
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(1,1)]
+        [InlineData(2,1)]
+        [InlineData(3,2)]
+        [InlineData(4,3)]
+        [InlineData(5,5)]
+        [InlineData(6,8)]
+        public void ComputesNthNumberInFibonacciSequence(int n, int expected)
+        {
+            Assert.Equal(expected, Fibonacci.Compute(n));
+        } 
+    }
+}
