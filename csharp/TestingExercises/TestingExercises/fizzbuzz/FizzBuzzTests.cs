75e4d578c515416f37296cc884ce7d3f6b2f0abf
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-03-17 09:16:03 +0100

    Fizzbuzz

---
+++ csharp/TestingExercises/TestingExercises/fizzbuzz/FizzBuzzTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingExercises.fizzbuzz
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void WriteYourTestsHere()
+        {
+            Assert.Fail("TODO: test-drive a fizzbuzz implementation");
+        }
+    }
+}
